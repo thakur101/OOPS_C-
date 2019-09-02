3e4b8716e6778f115743c3d9a55a9e6fd0d527f3
Abhishek Thakur <abhishek140597at@gmail.com>
2019-09-02 22:29:43 +0530

    User defined Type Conversion  OOPS.

--- Object_2_float.cpp
+++ Object_2_float.cpp
@@ -34,12 +34,14 @@ class Convert{
 
 int main(){
     Convert c1(2.35F);
-    
+    float mtr;
     c1.display();
     cout<<endl;
-    float mtr = static_cast<float>(c1);
+    mtr = static_cast<float>(c1);
     cout<<mtr;
-    
+    Distance d2(5, 10.25);
+    mtr = d2; //Note the calling of operator in a unique way.
+    cout<<mtrs<< " is the distance in meters"<<endl;
     return 0;
 
 }
